tied covariance fix lab07 implemented

src/modules/data/dataset.py
@@ -48,7 +48,11 @@ def load(filename: str, delimiter: str = ",") -> tuple[ndarray, ndarray, dict]:
             L = np.append(L, label_dict[label])
 
     # if binary problem with class 0 and 1, transform in True and False their labels name
-    if all([True if value=="0" or value=="1" else False for value in label_dict.keys()]):
+    if all([True if key=="0" or key=="1" else False for key in label_dict.keys()]):
+        if label_dict["1"] == 0:
+            # must invert the labels
+            L = np.array([1 if l == 0 else 0 for l in L])
+
         label_dict = {}
         label_dict["False"] = 0
         label_dict["True"] = 1

src/modules/models/gaussians.py
@@ -1,27 +1,43 @@
 import numpy as np
 from scipy.special import logsumexp
 from modules.utils.operations import col, mean, cov
+from modules.models.model import Model
 
-class GaussianModel:
-    def __init__(self, D, L, label_dict, l_priors = None):
+class GaussianModel(Model):
+    def __init__(self, D, L, label_dict, l_priors = None, cost_matrix = None):
         self.D = D
         self.L = L
         self.label_dict = label_dict
-        #??
-        #self.parameters = {}
-        # used for calibration (futher developement to do)
+
+        size = len(self.label_dict)
+        if size == 1:
+            raise Exception("Classes must be at least two")
+        if size != np.unique(L).size:
+            raise Exception("There's some missing class in your split")
+        
+        # if label_dict is not {key_00: 0, key_01: 1}, the problem is not considered binary
+        vals = list(label_dict.values())
+        self.binary = (vals.count(0) == 1 and vals.count(1) == 1 and size == 2)
+
+        if l_priors is not None:
+            l_priors = col(l_priors)
         self.l_priors = l_priors
+
+        if cost_matrix is None:
+            self.cost_matrix = np.ones((len(label_dict), len(label_dict)))
+            np.fill_diagonal(self.cost_matrix, 0)
+        else:
+            self.cost_matrix = cost_matrix
 
     def fit(self):
         pass
 
-    # TODO: insert the binary stuff
-    # TODO: insert prior stuff
     def debug_predict(self, D):
-        # return everything
-        # calculate log likelihoods and log priors of each class
-        # list comprehension (usually faster than for loops) and vectorized operation (is actually the fastest, probably going to keep this function as the main one)
-        results = [(logpdf_GAU_ND(D, self.parameters[label_int][0], self.parameters[label_int][1]), np.log(self.L[self.L==label_int].size/self.L.size)) for label_int in self.label_dict.values()]
+        if self.l_priors is None:
+            results = [(logpdf_GAU_ND(D, self.parameters[label_int][0], self.parameters[label_int][1]), np.log(self.L[self.L==label_int].size/self.L.size)) for label_int in self.label_dict.values()]
+        else:
+            results = [(logpdf_GAU_ND(D, self.parameters[label_int][0], self.parameters[label_int][1]), self.l_priors[label_int]) for label_int in self.label_dict.values()]
+
         l_likelihoods, l_priors = zip(*results)
         l_likelihoods = np.array(l_likelihoods)
         l_priors = col(np.array(l_priors))
@@ -33,65 +49,89 @@ class GaussianModel:
         l_marginals = logsumexp(l_joints, axis=0)
         # calculate log posterior probability
         l_posteriors = l_joints - l_marginals
-
-        # get the prediction index and change the value with the integer associated with that specific class
-        predictions = np.argmax(l_posteriors, axis=0)
+        # get costs
+        b_costs = expected_bayes_cost(self.cost_matrix, l_posteriors, is_log=True)
+        # get the prediction
+        predictions = np.argmin(b_costs, axis=0)
         values = list(self.label_dict.values())
         predictions = np.array([values[p] for p in predictions])
         
-        # return the exponential value of the log likelihood, the argmax (the most likely to be the belonging class) and the other log probabilities (just to check)
         results = {
             "l_likelihoods": l_likelihoods,
             "l_marginals": l_marginals,
             "l_posteriors": l_posteriors,
-            "l_joints": l_joints
+            "l_joints": l_joints,
+            "b_costs": b_costs
         }
 
         return predictions, results
 
-    # TODO: separate the llr calculation from the prediction, as suggested by the professor in lab05
-    def predict(self, D):
-        # return just scores and prediction
-        binary = (len(self.label_dict) == 2)
+    def predict(self, D, bin = None):
+        if bin is None:
+            bin = self.binary
+        l_scores = self.get_scores(D, bin)
+        predictions = self.get_predictions(l_scores, bin)
+        return predictions, l_scores
+    
+    def get_scores(self, D, bin = None):
+        if bin is None:
+            bin = self.binary
 
-        # calculate log likelihoods and log priors of each class
-        # if present, use the priors already provided
+        l_likelihoods = np.array([logpdf_GAU_ND(D, self.parameters[label_int][0], self.parameters[label_int][1]) for label_int in self.label_dict.values()])
+
+        if bin:
+            # PUT ALWAYS THE CLASS 1 AT THE NUMERATOR AND THE CLASS 0 AT THE DENOMINATOR (JUST FOR INTERPRETATION PURPOSES)
+            true_idx = list(self.label_dict.values()).index(1)
+            false_idx = (true_idx + 1 ) % 2
+
+            # loglikelihood ratios
+            # llr = log(P(x|true)/P(x|false)) (where true = 1, and false = 0)
+            l_scores = l_likelihoods[true_idx, :] - l_likelihoods[false_idx, :]
+        else:
+            # loglikelihoods
+            l_scores = l_likelihoods
+        
+        return l_scores
+
+    def get_predictions(self, l_scores, bin = None):
+        if bin is None:
+            bin = self.binary
+
         if self.l_priors is None:
-            # list comprehension (usually faster than for loops) and vectorized operation (is actually the fastest, probably going to keep this function as the main one)
-            results = [(logpdf_GAU_ND(D, self.parameters[label_int][0], self.parameters[label_int][1]), np.log(self.L[self.L==label_int].size/self.L.size)) for label_int in self.label_dict.values()]
-        else:
-            results = [(logpdf_GAU_ND(D, self.parameters[label_int][0], self.parameters[label_int][1]), self.l_priors[label_int]) for label_int in self.label_dict.values()]
+            self.l_priors = col(np.array([np.log(self.L[self.L==label_int].size/self.L.size) for label_int in self.label_dict.values()]))
         
-        l_likelihoods, l_priors = zip(*results)
-        l_likelihoods = np.array(l_likelihoods)
-        l_priors = col(np.array(l_priors))
-
-        #print(f"check: {np.all((logpdf_GAU_ND(D, self.parameters[1][0], self.parameters[1][1]) == l_likelihoods[0,:]))}")
-        
-        # ma conviene davvero?
         values = list(self.label_dict.values())
-        if binary:
-            # llr = log(P(x|0)/P(x|1))
-            llr = l_likelihoods[0, :] - l_likelihoods[1, :]
-            # t = - log(P(0)/P(1))
-            threshold =  l_priors[1, 0] - l_priors[0, 0]
-            l_scores = llr - threshold
+        if bin:
+            # PUT ALWAYS THE CLASS 1 AT THE NUMERATOR AND THE CLASS 0 AT THE DENOMINATOR
+            true_idx = values.index(1)
+            false_idx = (true_idx + 1 ) % 2
 
-            predictions = np.empty(llr.shape)
-            predictions[l_scores >= 0] = values[0]
-            predictions[l_scores < 0] = values[1]
+            priors = np.exp(self.l_priors)
+            threshold = -1 * np.log(priors[true_idx]*self.cost_matrix[false_idx, true_idx]/
+                                    (priors[false_idx]*self.cost_matrix[true_idx, false_idx]))
+            
+            predictions = np.empty(l_scores.shape)
+            predictions[l_scores > threshold] = values[true_idx]
+            predictions[l_scores <= threshold] = values[false_idx]
+
         else:
-            l_scores = l_likelihoods + l_priors
-            predictions = np.argmax(l_scores, axis=0)
+            l_posteriors = l_scores +  self.l_priors
+            b_costs = expected_bayes_cost(self.cost_matrix, l_posteriors, is_log=True)
+            predictions = np.argmin(b_costs, axis=0)
             predictions = np.array([values[p] for p in predictions])
         
-        return predictions, l_scores
+        return predictions.astype(int)
+    
+    # TODO: check if binary?
+    def set_threshold_from_priors_binary(self, t_prior):
+        self.l_priors = col(np.log([(1-t_prior), t_prior]))
+        self.cost_matrix = np.ones((2,2))
+        np.fill_diagonal(self.cost_matrix, 0)
 
 class MVGModel(GaussianModel):
     def fit(self):
-        # what if, in the split, there's no item belonging to one class?
         parameters = {}
-        for _, label_int in self.label_dict.items():
+        for label_int in self.label_dict.values():
             Dc = self.D[:,self.L==label_int]
             mu = mean(Dc)
             C = cov(Dc)
@@ -101,9 +141,8 @@ class MVGModel(GaussianModel):
 
 class NaiveGModel(GaussianModel):    
     def fit(self):
-        # what if, in the split, there's no item belonging to one class?
         parameters = {}
-        for _, label_int in self.label_dict.items():
+        for label_int in self.label_dict.values():
             Dc = self.D[:,self.L==label_int]
             mu = mean(Dc)
             C = np.identity(Dc.shape[0]) * cov(Dc)
@@ -113,19 +152,19 @@ class NaiveGModel(GaussianModel):
 
 class TiedGModel(GaussianModel):
     def fit(self):
-        # what if, in the split, there's no item belonging to one class?
         parameters = {}
-        C = cov(self.D)
-        for _, label_int in self.label_dict.items():
+        C = 0
+        for label_int in self.label_dict.values():
             Dc = self.D[:,self.L==label_int]
             mu = mean(Dc)
-            p = (mu, C)
-            parameters[label_int] = p
-        self.parameters = parameters
+            Dc = Dc - mu
+            parameters[label_int] = mu
+            C += np.dot(Dc, Dc.T)
+        C /= self.D.shape[1]
+        self.parameters = {k:(mu, C) for (k, mu) in parameters.items()}
 
-# TODO: change x in D (since it works for a whole dataset)
-# TODO: find a way to apply the same calculation if there's a series of parameters (means and covariance matrices). In this way
-    # externally can be applied the logpdf_GAU_ND without any for loop for each parameter member
+# TODO: implement TiedNaive
+
 def logpdf_GAU_ND(x, mu = None, C = None):
     if np.isscalar(C):
         logdet = np.log(C)
@@ -135,8 +174,14 @@ def logpdf_GAU_ND(x, mu = None, C = None):
         inv = np.linalg.inv(C)
     M = x.shape[0]
     x_c = x - mu
-    # fix the fact that C could be even a scalar
     return -(M/2) * np.log(2*np.pi) - .5 * logdet - .5 * (np.dot(x_c.T, inv).T * x_c).sum(0)
 
 def loglikelihood(X, mu, C):
     return logpdf_GAU_ND(X, mu, C).sum()
+
+# TODO: move??
+def expected_bayes_cost(C, posteriors, is_log = False):
+    if is_log is True:
+        posteriors = np.exp(posteriors)
+    
+    return np.dot(C, posteriors)